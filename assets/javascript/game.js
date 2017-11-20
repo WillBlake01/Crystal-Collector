2f4ca2c698ad8ae63df989609b85a85ff8c57fd3
William Blake <willblakebooking@gmail.com>
2017-11-20 10:37:47 -0600

    some functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,56 @@
 //gems random number between 1-12
 //solution number random number between 19-120
 
+$(document).ready(function(){
+
 //Gem value array
-var gemValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+var gemValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+var solutionValue = ["19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
+"29","30", "31", "32", "33", "34", "35", "36", "37", "38", "39","40", "41", "42",
+"43", "44", "45", "46", "47", "48", "49","50", "51", "52", "53", "54", "55", "56",
+"57", "58", "59","60", "61", "62", "63", "64", "65", "66", "67", "68", "69","70",
+"71", "72", "73", "74", "75", "76", "77", "78", "79","80", "81", "82", "83", "84",
+"85", "86", "87", "88", "89","90", "91", "92", "93", "94", "95", "96", "97", "98",
+"99","100", "101", "102", "103", "104", "105", "106", "107", "108", "109","110", 
+"111", "112", "113", "114", "115", "116", "117", "118", "119","120"];
 
 var solution = "";
-var score = 0;
+var scoreReset = 0;
+var score = "";
 var wins = "";
 var losses = "";
 var redGem = "";
 var greenGem = "";
 var blueGem = "";
 var pinkGem = "";
+//Set gem values
+var gemsRandom = gemValue[Math.floor(Math.random()*gemValue.length -1)];
+var pickSolution = solutionValue[Math.floor(Math.random()*solutionValue.length -1)];
 
+//Assign a solution
+$("button").on("click", function() {
+	$("div#solution").html("<h1>" + pickSolution + "</h1>");
+});
 
-//Add players score
-$("div#score-number").html("<h1>" + score + "</h1>");
+//Assign to buttons
+$("button#red-gem").on("click", function() {
+	$("div#score-number").html("<h1>" + score + gemsRandom + "</h1>");
+})
+
+$("button#green-gem").on("click", function() {
+	$("div#score-number").html("<h1>" + score + gemsRandom + "</h1>");
+})
+
+$("button#blue-gem").on("click", function() {
+	$("div#score-number").html("<h1>" + score + gemsRandom + "</h1>");
+})
+
+$("button#pink-gem").on("click", function() {
+	$("div#score-number").html("<h1>" + score + gemsRandom + "</h1>");
+})
+
+//Display users current score
+$("div#score-number").html("<h1>" + scoreReset + "</h1>");
+
+})
