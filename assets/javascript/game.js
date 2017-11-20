6e6ac370af18e0d2cea96d23a56cdfb72255b20f
William Blake <willblakebooking@gmail.com>
2017-11-19 19:16:30 -0600

    some javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,18 @@
 //gems random number between 1-12
 //solution number random number between 19-120
+
+//Gem value array
+var gemValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+
+var solution = "";
+var score = 0;
+var wins = "";
+var losses = "";
+var redGem = "";
+var greenGem = "";
+var blueGem = "";
+var pinkGem = "";
+
+
+//Add players score
+$("div#score-number").html("<h1>" + score + "</h1>");
