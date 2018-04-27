d8b8d8d50ccd5f934b8cc6a106d7700f8655eb5b
William Blake <willblakebooking@gmail.com>
2018-04-27 16:21:13 -0500

    javascript and css fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,90 +3,113 @@
 
 $(document).ready(function(){
 
-//Gem value array
-var gemValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+	var yourMatchingNumber = 0;
+	var randomNum = randomNumGen();
 
-var solutionValue = ["19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-"29","30", "31", "32", "33", "34", "35", "36", "37", "38", "39","40", "41", "42",
-"43", "44", "45", "46", "47", "48", "49","50", "51", "52", "53", "54", "55", "56",
-"57", "58", "59","60", "61", "62", "63", "64", "65", "66", "67", "68", "69","70",
-"71", "72", "73", "74", "75", "76", "77", "78", "79","80", "81", "82", "83", "84",
-"85", "86", "87", "88", "89","90", "91", "92", "93", "94", "95", "96", "97", "98",
-"99","100", "101", "102", "103", "104", "105", "106", "107", "108", "109","110", 
-"111", "112", "113", "114", "115", "116", "117", "118", "119","120"];
+	// Setting up starting variables
+	var wins = 0;
+	var losses = 0;
+	var crystals;
 
-var scoreReset = 0;
-var score = "";
-var wins = "";
-var losses = "";
-
-//Set gem values
-var redValue = gemValue[Math.floor(Math.random()*gemValue.length)];
-var greenValue = gemValue[Math.floor(Math.random()*gemValue.length)];
-var blueValue = gemValue[Math.floor(Math.random()*gemValue.length)];
-var pinkValue = gemValue[Math.floor(Math.random()*gemValue.length)];
-var solution = solutionValue[Math.floor(Math.random()*solutionValue.length)];
-
-//Reset score to zero
-$("div#score-number").html("<h1>" + scoreReset + "</h1>");
-
-//Assign a solution
-$("button").on("click", function pickSolution() {
-	solution = parseInt(solution);
-	$("div#solution").html("<h1>" + solution + "</h1>");
-});
-
-//Assign to buttons
-$("button#red-gem").on("click", function redclick() {
-	redValue = parseInt(redValue);
-	score = parseInt(score + redValue);
-	$("div#score-number").html("<h1>" + score + "</h1>");
-	if (score > solution) {
-		$("div#result").html("<p>" + "You Lose!" + "</p>");
-		losses++;
-	} else if (score == solution) {
-		$("div#result").html("<p>" + "You Win!" + "</p>");
-		wins++;
+	function randomNumCrystals() {
+		return {
+			red: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: 'assets/images/red-gem.jpg'
+			},
+			blue: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: 'assets/images/blue-gem.jpg'
+			},
+			green: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: 'assets/images/green-gem.jpg'
+			},
+			pink: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: 'assets/images/pink-gem.jpg'
+			}
+		};
 	}
-});
 
-$("button#green-gem").on("click", function greenclick() {
-	greenValue = parseInt(greenValue);
-	score = parseInt(score + greenValue);
-	$("div#score-number").html("<h1>" + score + "</h1>");
-	if (score > solution) {
-		$("div#result").html("<p>" + "You Lose!" + "</p>");
-		losses++;
-	} else if (score == solution) {
-		$("div#result").html("<p>" + "You Win!" + "</p>");
-		wins++;
+	// Function to create a number between 19 and 120.
+	function randomNumGen() {
+		return Math.floor(Math.random() * 102) + 19;
 	}
-});
 
-$("button#blue-gem").on("click", function blueclick() {
-	blueValue = parseInt(blueValue);
-	score = parseInt(score + blueValue);
-	$("div#score-number").html("<h1>" + score + "</h1>");
-	if (score > solution) {
-		$("div#result").html("<p>" + "You Lose!" + "</p>");
-		losses++;
-	} else if (score == solution) {
-		$("div#result").html("<p>" + "You Win!" + "</p>");
-		wins++;
+	function setGame() {
+		yourMatchingNumber = 0;
+		crystals = randomNumCrystals();
+		randomNum = randomNumGen();
+		$('#solution').html('<h1>' + randomNum + '</h1>');
 	}
-});
 
-$("button#pink-gem").on("click", function pinkclick() {
-	pinkValue = parseInt(pinkValue);
-	score = parseInt(score + pinkValue);
-	$("div#score-number").html("<h1>" + score + "</h1>");
-	if (score > solution) {
-		$("div#result").html("<p>" + "You Lose!" + "</p>");
-		losses++;
-	} else if (score == solution) {
-		$("div#result").html("<p>" + "You Win!" + "</p>");
-		wins++;
+	function updateDom(didUserWin) {
+		$('#wins-losses').empty();
+
+		if (didUserWin === true) {
+			$('#wins-losses').append($('<p>').text('You won!!'));
+			setGame();
+			renderMatchingNumber();
+		} else if (didUserWin === false) {
+			$('#wins-losses').append($('<p>').text('You lost!!'));
+			setGame();
+			renderMatchingNumber();
+		}
+
+		var wSpan = $('<span>').text(wins);
+		var lSpan = $('<span>').text(losses);
+
+		var pWins = $('<p>').text('Wins: ');
+		var pLosses = $('<p>').text('Losses: ');
+
+		pWins.append(wSpan);
+		pLosses.append(lSpan);
+
+		$('#wins-losses').append(pWins);
+		$('#wins-losses').append(pLosses);
 	}
-});
 
+	// Function to render crystals
+	function renderCrystals() {
+		for (var key in crystals) {
+			var crystalDiv = $("<div class='crystals-button' data-name='" + key + "'>");
+			var crystalImg = $("<img alt='image' class='crystal-img'>").attr('src', crystals[key].imageUrl);
+			crystalDiv.append(crystalImg);
+			$('#crystal-area').append(crystalDiv);
+		}
+	}
+
+	function updateMatchingNumber(crystal) {
+		yourMatchingNumber += crystals[crystal.attr('data-name')].points;
+	}
+
+	function renderMatchingNumber() {
+		var scoreNumDiv = $("<div id='score-number'>").html('<h1>' + yourMatchingNumber + '</h1>');
+		$('#score-area').html();
+		$('#score-area').html(scoreNumDiv);
+	}
+
+	// Call functions to start game
+	setGame();
+	updateDom();
+	renderCrystals();
+	renderMatchingNumber();
+
+	// on.click event for the crystals
+	$('.crystals-button').on('click', function(event) {
+		// Update current guess number and render
+		updateMatchingNumber($(this));
+		renderMatchingNumber();
+
+		if (yourMatchingNumber === randomNum) {
+			wins++;
+			setGame();
+			updateDom(true);
+		} else if (yourMatchingNumber > randomNum) {
+			losses++;
+			setGame();
+			updateDom(false);
+		}
+	});
 });
